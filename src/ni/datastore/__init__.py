786223bdf7ad5cc91aabb09d9e4496baaf032de5
hunter-ni <68388297+hunter-ni@users.noreply.github.com>
2025-09-26 10:23:45 -0500

    Expose Step, TestResult, and PublishedMeasurement wrapper types using Hightime for timestamps (#24)
    * Adding PublishedMeasurement, Step, and TestResult messages that make use of hightime and wrap their corresponding protobuf messages.
    
    * Continued implementation of wrapper types
    
    * Resolving test error after merge from main
    
    * Updating notebooks
    
    * Fixing test failures
    
    * Add from __future__ import annotations to data.py
    
    * Fixing linter issues
    
    * Fix code analysis issues
    
    * Fix code analysis issue
    
    * Fix code analysis issues
    
    * Rolling back changes
    
    * Revert "Rolling back changes"
    
    This reverts commit 58b5aa4b0803fe07bf707d8125f0915ac81066c4.
    
    * Making PublishedMeasurement.Moniker also allow a 'None' value (to match the same property on the corresponding gRPC type)
    
    * Renaming use of hightime_datetime to datetime
    
    * Making TestResult.Outcome read-only
    
    * Rename for clarity
    
    * Adding additional fields to Step equality check.
    
    * Linting issue
    
    * Review feedback - remove unnecessary conditional assignment
    
    * Review feedback - updating test assertions
    
    * Review feedback - simplifying imports of Client
    
    * Review feedback - separate PublishedMeasurement, Step, and TestResult into their own modules. Expose them as part of the 'ni.datastore' package.
    
    * Update overview example after merge form main
    
    * Review feedback - Prefixing usages of hightime.datetime with ht
    
    * Adding __init__.py for 'types' sub-package
    
    * Review feedback - Update notebook/example usages of standard datetime in waveforms to now use hightime.
    
    * Review feedback - add py.typed to 'types' folder/sub-package
    
    * Fix analysis issue
    
    * Review feedback - Update to use hightime.timedelta
    
    * Review feedback - revert version changes
    
    * Review feedback - separate out contents of __all__ onto different lines
    
    ---------
    
    Signed-off-by: Hunter Kennon hunter.kennon@emerson.com
    
    Co-authored-by: Joel Dixon <38357562+dixonjoel@users.noreply.github.com>

--- src/ni/datastore/__init__.py
+++ src/ni/datastore/__init__.py
@@ -1,5 +1,13 @@
 """Public API for accessing the NI Data Store Service."""
 
 from ni.datastore.client import Client
+from ni.datastore.types._published_measurement import PublishedMeasurement
+from ni.datastore.types._step import Step
+from ni.datastore.types._test_result import TestResult
 
-__all__ = ["Client"]
+__all__ = [
+    "Client",
+    "Step",
+    "TestResult",
+    "PublishedMeasurement",
+]
