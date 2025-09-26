7d65a94c0903a0166b36660b1b591ea604f4ff4a
hunter-ni <68388297+hunter-ni@users.noreply.github.com>
2025-09-26 17:43:43 -0500

    Exposing remaining needed types in ni.datastore package.

--- src/ni/datastore/__init__.py
+++ src/ni/datastore/__init__.py
@@ -1,5 +1,6 @@
 """Public API for accessing the NI Data Store Service."""
 
+from ni.datamonikers.v1.data_moniker_pb2 import Moniker
 from ni.datastore.client import Client
 from ni.datastore.types._alias import Alias
 from ni.datastore.types._extension_schema import ExtensionSchema
@@ -16,13 +17,19 @@ from ni.datastore.types._test_result import TestResult
 from ni.datastore.types._test_station import TestStation
 from ni.datastore.types._uut import Uut
 from ni.datastore.types._uut_instance import UutInstance
+from ni.measurements.data.v1.data_store_pb2 import ErrorInformation, Outcome
+from ni.measurements.metadata.v1.metadata_store_pb2 import AliasTargetType
 
 __all__ = [
     "Client",
     "Alias",
+    "AliasTargetType",
+    "ErrorInformation",
     "ExtensionSchema",
     "HardwareItem",
     "Operator",
+    "Moniker",
+    "Outcome",
     "PublishedCondition",
     "PublishedMeasurement",
     "SoftwareItem",
