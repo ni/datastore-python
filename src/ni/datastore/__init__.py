21a7669ec03df73c97ded7abbd3ecb2beff831b2
hunter-ni <68388297+hunter-ni@users.noreply.github.com>
2025-09-29 15:48:10 -0500

    Review feedback - Mark client.py as essentially an implementation detail by renaming it to _client.py. Mask imported types of ni.datastore as belonging to main module.

--- src/ni/datastore/__init__.py
+++ src/ni/datastore/__init__.py
@@ -1,7 +1,7 @@
 """Public API for accessing the NI Data Store Service."""
 
 from ni.datamonikers.v1.data_moniker_pb2 import Moniker
-from ni.datastore.client import Client
+from ni.datastore._client import Client
 from ni.datastore.types._alias import Alias
 from ni.datastore.types._extension_schema import ExtensionSchema
 from ni.datastore.types._hardware_item import HardwareItem
@@ -42,3 +42,25 @@ __all__ = [
     "Uut",
     "UutInstance",
 ]
+
+# Hide that it was not defined in this top-level package
+Client.__module__ = __name__
+Alias.__module__ = __name__
+AliasTargetType.__module__ = __name__
+ErrorInformation.__module__ = __name__
+ExtensionSchema.__module__ = __name__
+HardwareItem.__module__ = __name__
+Moniker.__module__ = __name__
+Operator.__module__ = __name__
+Outcome.__module__ = __name__
+PublishedCondition.__module__ = __name__
+PublishedMeasurement.__module__ = __name__
+SoftwareItem.__module__ = __name__
+Step.__module__ = __name__
+Test.__module__ = __name__
+TestAdapter.__module__ = __name__
+TestDescription.__module__ = __name__
+TestResult.__module__ = __name__
+TestStation.__module__ = __name__
+Uut.__module__ = __name__
+UutInstance.__module__ = __name__
