50d3cb27c49d52bee278f6219aea2766361cbd22
Michael Johansen <michael.johansen@emerson.com>
2025-10-28 10:14:46 -0500

    Add/expose query_test_results in the data store client
    Signed-off-by: Michael Johansen <michael.johansen@emerson.com>

--- tests/acceptance/_utils.py
+++ tests/acceptance/_utils.py
@@ -11,13 +11,19 @@ from ni.datastore.data import (
 
 def create_test_result_and_step(data_store_client: DataStoreClient, description: str) -> str:
     """Create a single step within a single test result and return the step_id."""
-    test_result_name = f"{description} test result"
-    test_result = TestResult(test_result_name=test_result_name)
-    test_result_id = data_store_client.create_test_result(test_result)
+    test_result_id = create_test_result(data_store_client, description)
 
     step = Step(step_name=f"{description} step", test_result_id=test_result_id)
     step_id = data_store_client.create_step(step)
     return step_id
+
+
+def create_test_result(data_store_client: DataStoreClient, description: str) -> str:
+    """Create a single TestResult and return that test result's id."""
+    test_result_name = f"{description} test result"
+    test_result = TestResult(test_result_name=test_result_name)
+    test_result_id = data_store_client.create_test_result(test_result)
+    return test_result_id
 
 
 def append_hashed_time(base_string: str) -> str:
