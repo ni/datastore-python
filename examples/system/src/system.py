f8e81d5b92a797f107c0cd9fb1fa778102e67509
Joe Friedrichsen <114173023+jfriedri-ni@users.noreply.github.com>
2025-11-06 12:10:36 -0600

    Add function that calls SysAPI and detects hardware and software

--- examples/system/src/system.py
+++ examples/system/src/system.py
@@ -1,9 +1,30 @@
 """How to detect, publish, and query system hardware and software resources."""
 
+import nisyscfg
+
 
 def main() -> None:
     """Detect, publish, and query hardware and software resources from the local system."""
-    print("Hello world")
+    hardware, software = detect_system_resources()
+
+
+def detect_system_resources(
+    system_target: str = "localhost",
+) -> tuple[dict[str, object], dict[str, object]]:
+    """Return available hardware and software resources on the system_target."""
+    hardware = {}
+    software = {}
+
+    with nisyscfg.Session(target=system_target) as session:
+        filter = session.create_filter()
+        filter.is_present = True
+        filter.is_ni_product = True
+        filter.is_device = True
+
+        hardware = {entry.name: entry for entry in session.find_hardware(filter)}
+        software = {entry.id: entry for entry in session.get_installed_software_components()}
+
+    return hardware, software
 
 
 if __name__ == "__main__":
