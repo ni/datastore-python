b38d053fbf1fdc9f79ca2f49a898563adf217095
hunter-ni <68388297+hunter-ni@users.noreply.github.com>
2025-11-17 17:40:52 -0600

    Remove '_example_context' from import statement of ExampleContext

--- examples/notebooks/initialize_example_notebook.py
+++ examples/notebooks/initialize_example_notebook.py
@@ -4,7 +4,7 @@ from pathlib import Path
 examples_directory = Path(__file__).resolve().parent.parent
 sys.path.append(str(examples_directory))
 
-from common._example_context import ExampleContext
+from common import ExampleContext
 
 example_context = ExampleContext()
 example_context.initialize()
